7d538371fefcdbccca463e78e5983c040262043c
Dan <dan.tyan@gmail.com>
2012-11-17 17:26:03 +0600

    added related label

--- models.py
+++ models.py
@@ -31,21 +31,30 @@ class CatalogCategory( CommonCategory ):
     pass
 
 class CatalogPost( CommonPost ):
-    category = models.ManyToManyField( CatalogCategory )
-    country = models.ForeignKey( Country, null = True, )
+    category = models.ManyToManyField( 
+        CatalogCategory,
+        related_name = "%(app_label)s_%(class)s_related",
+    )
+    country = models.ForeignKey( 
+        Country,
+        null = True,
+        default = '',
+        related_name = "%(app_label)s_%(class)s_related",
+    )
     city = ChainedForeignKey( 
         City,
         chained_field = "country",
         chained_model_field = "country",
         show_all = False,
-        auto_choose = False
+        auto_choose = False,
+        related_name = "%(app_label)s_%(class)s_related",
     )
     site = models.URLField( 
         blank = True,
         verbose_name = u'Site URL',
         help_text = u'your web site full URL',
     )
-    is_featured = models.NullBooleanField( default = 0 )
+    is_featured = models.BooleanField( default = 0 )
     featured_untill = models.DateTimeField( blank = True )
     email = models.EmailField( blank = True )
     phone = models.CharField( max_length = 30, blank = True )
@@ -59,17 +68,14 @@ class CatalogPost( CommonPost ):
         blank = True,
         help_text = u'full URL to your account with http://',
     )
-    google_plus = models.URLField(
+    google_plus = models.URLField( 
         blank = True,
         help_text = u'full URL to your account with http://',
     )
 
-
-#class CatalogPostMeta( models.Model ):
-#    post = models.ForeignKey( CatalogPost, blank = True, null = True, )
-#    key = models.CharField( max_length = 25, choices = META_CHOICE )
-#    value = models.TextField()
-
 class CatalogPostImages( CommonPostImage ):
-    post = models.ForeignKey( CatalogPost, default = '', blank = True, null = True, )
+    post = models.ForeignKey( 
+        CatalogPost,
+        related_name = "%(app_label)s_%(class)s_related",
+    )
     image = models.ImageField( upload_to = image_upload_to )
